15db5776749c38958d2218de01d0255e150381d5
sultanovaleyla <116901609+sultanovaleyla@users.noreply.github.com>
2023-06-23 23:13:56 +0300

    Delete labs/lab11/report/image directory

--- labs/lab11/report/image/112.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-gcc -o cprog 12.c
-./cprog
-case $? in
-0) echo "Число равно нулю";;
-1) echo "Число больше нуля";;
-2) echo "Число меньше нуля";;
-esac
--- labs/lab11/report/image/114.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-find $* -mtime -7 -mtime +0 -type f > FILES.txt
-tar -cf archive.tar -T FILES.txt
